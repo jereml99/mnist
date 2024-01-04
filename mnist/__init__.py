2aaeb78a4dc5ab898e49511cc0d40fd8462a7f0f
jereml99 <ledwojeremi@gmail.com>
2024-01-04 11:30:07 +0100

    Refactor model architecture in model.py

--- mnist/__init__.py
+++ mnist/__init__.py
@@ -1,2 +1,2 @@
-from mnist.models.model import MyNeuralNet
+from mnist.models.model import MyAwesomeModel
 from mnist.predict_model import predict
--- mnist/models/model.py
+++ mnist/models/model.py
@@ -1,26 +1,18 @@
-import torch
+from torch import nn
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
+
+class MyAwesomeModel(nn.Module):
+    """My awesome model."""
+
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(784, 128), # [B, ]
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        ) 
+
+    def forward(self, x):
+        """Forward pass of the model."""
         
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
-
-        Returns:
-            Output tensor with shape [N,out_features]
-
-        """
-        return self.l2(self.r(self.l1(x)))
+        return self.model(x.flatten(1))
