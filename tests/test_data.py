dc01b978e69c1d3cc35d8bbab16eac8ff32a3571
jereml99 <ledwojeremi@gmail.com>
2024-01-08 19:03:14 +0100

    Add test files for data module

---
+++ tests/test_data.py
@@ -0,0 +1,15 @@
+from collections import Counter
+from mnist.data.dataloader import make_training_dataloader
+
+
+def test_data():
+    batch_size = 100
+    dataloader = make_training_dataloader(batch_size)
+    assert len(dataloader.dataset) == 50000
+    label_counter = Counter()
+
+    for data, label in dataloader:
+        assert data.shape == (batch_size, 1, 28, 28)
+        label_counter.update(label.tolist())
+
+    assert len(label_counter) == 10 # we have 10 diffrent digits in the dataset
